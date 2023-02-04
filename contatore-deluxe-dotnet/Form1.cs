3fe194ca2692144b04d2c8486ec96ddf61172eb1
AleG <aleguizzo@icloud.com>
2023-02-04 14:46:11 +0100

    controllo File Finti.

--- contatore-deluxe-dotnet/Form1.cs
+++ contatore-deluxe-dotnet/Form1.cs
@@ -6,11 +6,14 @@ namespace contatore_deluxe_dotnet
     public partial class Form1 : Form
     {
         string programVersion = "1.0";
-        string buildType = "Beta";
+        string buildType = "Release";
+        string[] devBuilds = new string[] {"Developement", "Release Candidate", "Beta", "Master", "Alpha"};
+        // create a string array
+        
         public Form1()
         {
             InitializeComponent();
-            if (buildType != "Beta")
+            if (!((IList<string>)devBuilds).Contains(buildType))
             {
                 debugToolStripMenuItem.Visible = false;
             }
@@ -518,114 +521,126 @@ namespace contatore_deluxe_dotnet
                     string player3NameFile = fileLoader.ReadLine();
                     string player4NameFile = fileLoader.ReadLine();
                     string actionHistoryFile = fileLoader.ReadLine();
+                    string copyrightFileEnd = fileLoader.ReadLine();
+                    string endOfFile = fileLoader.ReadLine();
                     fileLoader.Close();
-                    int playerCountFile = 0;
-                    if (players_2)
+                    if (copyrightFile == copyrightFileEnd && endOfFile == "endoffile-endoffile")
                     {
-                        playerCountFile = 2;
-                    } else if (players_3)
-                    {
-                        playerCountFile = 3;
-                    } else if (players_4)
-                    {
-                        playerCountFile = 4;
-                    }
-                        
-                    var result = MessageBox.Show($"Caricato file: {filePath}\n" +
-                        $"Versione ContatoreDELUXE: {originalBuildType} {originalVersion}\n" +
-                        $"Tema colorato abilitato: {temaColorato.ToString()}\n" +
-                        $"Mostra logo ContatoreDELUXE: {mostraLogo.ToString()}\n" +
-                        $"Numero giocatori: {playerCountFile.ToString()}\n" +
-                        $"Si vince a: {winnerThresholdFile.ToString()}\n" +
-                        $"Giocatore 1: {player1NameFile} con {player1CountFile.ToString()}\n" +
-                        $"Giocatore 2: {player2NameFile} con {player2CountFile.ToString()}\n" +
-                        $"Giocatore 3: {player3NameFile} con {player3CountFile.ToString()}\n" +
-                        $"Giocatore 4: {player4NameFile} con {player4CountFile.ToString()}\n" +
-                        $"Si vuole caricare il file e sovrascrivere la partita in corso?", "Caricamento file riuscito.", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                    if (result == DialogResult.Yes)
-                    {
-                        player1Count = player1CountFile;
-                        count1.Text = player1CountFile.ToString("000");
-                        player1TextBox.Text = player1NameFile;
-                        player2Count = player2CountFile;
-                        count2.Text = player2CountFile.ToString("000");
-                        player2TextBox.Text = player2NameFile;
-                        player3Count = player3CountFile;
-                        count3.Text = player3CountFile.ToString("000");
-                        player3TextBox.Text = player3NameFile;
-                        player4Count = player4CountFile;
-                        count4.Text = player4CountFile.ToString("000");
-                        player4TextBox.Text = player4NameFile;
-                        temaColoratoToolStripMenuItem.Checked = temaColorato;
-                        if (temaColoratoToolStripMenuItem.Checked)
+                        int playerCountFile = 0;
+                        if (players_2)
                         {
-                            count1.ForeColor = Color.Blue;
-                            count2.ForeColor = Color.Red;
-                            count3.ForeColor = Color.LimeGreen;
-                            count4.ForeColor = Color.DarkViolet;
-                            player1TextBox.ForeColor = Color.Blue;
-                            player2TextBox.ForeColor = Color.Red;
-                            player3TextBox.ForeColor = Color.LimeGreen;
-                            player4TextBox.ForeColor = Color.DarkViolet;
-                            player1Up.ForeColor = Color.Blue;
-                            player1Down.ForeColor = Color.Blue;
-                            player2Up.ForeColor = Color.Red;
-                            player2Down.ForeColor = Color.Red;
-                            player3Up.ForeColor = Color.LimeGreen;
-                            player3Down.ForeColor = Color.LimeGreen;
-                            player4Up.ForeColor = Color.DarkViolet;
-                            player4Down.ForeColor = Color.DarkViolet;
+                            playerCountFile = 2;
                         }
-                        else
+                        else if (players_3)
                         {
-                            count1.ForeColor = Color.Black;
-                            count2.ForeColor = Color.Black;
-                            count3.ForeColor = Color.Black;
-                            count4.ForeColor = Color.Black;
-                            player1TextBox.ForeColor = Color.Black;
-                            player2TextBox.ForeColor = Color.Black;
-                            player3TextBox.ForeColor = Color.Black;
-                            player4TextBox.ForeColor = Color.Black;
-                            player1Up.ForeColor = Color.Black;
-                            player1Down.ForeColor = Color.Black;
-                            player2Up.ForeColor = Color.Black;
-                            player2Down.ForeColor = Color.Black;
-                            player3Up.ForeColor = Color.Black;
-                            player3Down.ForeColor = Color.Black;
-                            player4Up.ForeColor = Color.Black;
-                            player4Down.ForeColor = Color.Black;
+                            playerCountFile = 3;
                         }
-                        changePlayerCount(playerCountFile);
-                        mostraLogoToolStripMenuItem.Checked = mostraLogo;
-                        if (mostraLogoToolStripMenuItem.Checked)
+                        else if (players_4)
                         {
-                            if (giocatoriToolStripMenuItem.Checked)
+                            playerCountFile = 4;
+                        }
+
+                        var result = MessageBox.Show($"Caricato file: {filePath}\n" +
+                            $"Versione ContatoreDELUXE: {originalBuildType} {originalVersion}\n" +
+                            $"Tema colorato abilitato: {temaColorato.ToString()}\n" +
+                            $"Mostra logo ContatoreDELUXE: {mostraLogo.ToString()}\n" +
+                            $"Numero giocatori: {playerCountFile.ToString()}\n" +
+                            $"Si vince a: {winnerThresholdFile.ToString()}\n" +
+                            $"Giocatore 1: {player1NameFile} con {player1CountFile.ToString()}\n" +
+                            $"Giocatore 2: {player2NameFile} con {player2CountFile.ToString()}\n" +
+                            $"Giocatore 3: {player3NameFile} con {player3CountFile.ToString()}\n" +
+                            $"Giocatore 4: {player4NameFile} con {player4CountFile.ToString()}\n" +
+                            $"Si vuole caricare il file e sovrascrivere la partita in corso?", "Caricamento file riuscito.", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (result == DialogResult.Yes)
+                        {
+                            player1Count = player1CountFile;
+                            count1.Text = player1CountFile.ToString("000");
+                            player1TextBox.Text = player1NameFile;
+                            player2Count = player2CountFile;
+                            count2.Text = player2CountFile.ToString("000");
+                            player2TextBox.Text = player2NameFile;
+                            player3Count = player3CountFile;
+                            count3.Text = player3CountFile.ToString("000");
+                            player3TextBox.Text = player3NameFile;
+                            player4Count = player4CountFile;
+                            count4.Text = player4CountFile.ToString("000");
+                            player4TextBox.Text = player4NameFile;
+                            temaColoratoToolStripMenuItem.Checked = temaColorato;
+                            if (temaColoratoToolStripMenuItem.Checked)
                             {
-                                this.BackgroundImage = Properties.Resources.contatoredeluxe_small;
+                                count1.ForeColor = Color.Blue;
+                                count2.ForeColor = Color.Red;
+                                count3.ForeColor = Color.LimeGreen;
+                                count4.ForeColor = Color.DarkViolet;
+                                player1TextBox.ForeColor = Color.Blue;
+                                player2TextBox.ForeColor = Color.Red;
+                                player3TextBox.ForeColor = Color.LimeGreen;
+                                player4TextBox.ForeColor = Color.DarkViolet;
+                                player1Up.ForeColor = Color.Blue;
+                                player1Down.ForeColor = Color.Blue;
+                                player2Up.ForeColor = Color.Red;
+                                player2Down.ForeColor = Color.Red;
+                                player3Up.ForeColor = Color.LimeGreen;
+                                player3Down.ForeColor = Color.LimeGreen;
+                                player4Up.ForeColor = Color.DarkViolet;
+                                player4Down.ForeColor = Color.DarkViolet;
                             }
-                            else if (giocatoriToolStripMenuItem1.Checked || giocatoriToolStripMenuItem2.Checked)
+                            else
                             {
-                                this.BackgroundImage = Properties.Resources.contatoredeluxe;
+                                count1.ForeColor = Color.Black;
+                                count2.ForeColor = Color.Black;
+                                count3.ForeColor = Color.Black;
+                                count4.ForeColor = Color.Black;
+                                player1TextBox.ForeColor = Color.Black;
+                                player2TextBox.ForeColor = Color.Black;
+                                player3TextBox.ForeColor = Color.Black;
+                                player4TextBox.ForeColor = Color.Black;
+                                player1Up.ForeColor = Color.Black;
+                                player1Down.ForeColor = Color.Black;
+                                player2Up.ForeColor = Color.Black;
+                                player2Down.ForeColor = Color.Black;
+                                player3Up.ForeColor = Color.Black;
+                                player3Down.ForeColor = Color.Black;
+                                player4Up.ForeColor = Color.Black;
+                                player4Down.ForeColor = Color.Black;
                             }
+                            changePlayerCount(playerCountFile);
+                            mostraLogoToolStripMenuItem.Checked = mostraLogo;
+                            if (mostraLogoToolStripMenuItem.Checked)
+                            {
+                                if (giocatoriToolStripMenuItem.Checked)
+                                {
+                                    this.BackgroundImage = Properties.Resources.contatoredeluxe_small;
+                                }
+                                else if (giocatoriToolStripMenuItem1.Checked || giocatoriToolStripMenuItem2.Checked)
+                                {
+                                    this.BackgroundImage = Properties.Resources.contatoredeluxe;
+                                }
+                            }
+                            else
+                            {
+                                this.BackgroundImage = null;
+                            }
+                            actionHistory = actionHistoryFile;
+                            toolStripTextBox1.Text = winnerThresholdFile.ToString(); ;
+                            winnerThreshold = winnerThresholdFile;
                         }
-                        else
-                        {
-                            this.BackgroundImage = null;
-                        }
-                        actionHistory = actionHistoryFile;
-                        toolStripTextBox1.Text = winnerThresholdFile.ToString(); ;
-                        winnerThreshold = winnerThresholdFile;
                     }
+                    else
+                    {
+                        MessageBox.Show($"Impossibile caricare il file {filePath}.\nMotivo: Formato file non conforme alle specifiche", "Errore F002", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
                 }
                 else
                 {
                     fileLoader.Close();
-                    MessageBox.Show($"Impossibile caricare il file {filePath}.\nMotivo: file non corretto", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show($"Impossibile caricare il file {filePath}.\nMotivo: File non compatibile", "Errore F001", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
             catch
             {
-                MessageBox.Show($"Impossibile caricare il file {filePath}.\nMotivo: errore non riconosciuto", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"Impossibile caricare il file {filePath}.\nMotivo: Errore generico", "Errore F000", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
         }
